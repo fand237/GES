5a9d832cc5c99806279181321df0605f9bef8504
fand237 <nzadouo@gmail.com>
2025-04-17 21:05:33 +0100

    modif de  config front

--- ges-front/src/config/config.js
+++ ges-front/src/config/config.js
@@ -1,6 +1,7 @@
+
 const config = {
     api: {
-        baseUrl: process.env.REACT_APP_API_URL,
+        baseUrl: process.env.REACT_APP_API_URL || "http://ges-production.up.railway.app"
     }
 };
 
