efcddd19cf127eec3acf38da3a4bc7bff5d3e0bc
fand237 <nzadouo@gmail.com>
2025-04-17 21:34:50 +0100

    modif de  config front with https pour le back

--- ges-front/src/config/config.js
+++ ges-front/src/config/config.js
@@ -1,7 +1,7 @@
 
 const config = {
     api: {
-        baseUrl: process.env.REACT_APP_API_URL || "http://ges-production.up.railway.app"
+        baseUrl: process.env.REACT_APP_API_URL || "https://ges-production.up.railway.app"
     }
 };
 
